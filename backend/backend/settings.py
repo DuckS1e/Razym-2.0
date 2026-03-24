20fbba1d5846931dda7e95d9cdd1ecce32670a51
soseschel <soseschel@yandex.ru>
2026-03-24 20:07:56 +0300

    настройки urls, создание суперюзера

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -90,8 +90,8 @@ DATABASES = {
 }
 
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000/',
-    'http://127.0.0.1:3000/',
+    'http://localhost:3000',
+    'http://127.0.0.1:3000',
 ]
 
 REST_FRAMEWORK = {
